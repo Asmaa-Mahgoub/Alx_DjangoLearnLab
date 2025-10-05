977574505faf6882169f46c183a9bf2d489f3aca
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-10-05 18:59:16 +0300

    Add UserCreationForm for registration and add email field and optional first_name & last_name to it

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
 This imports Django’s built-in User model, which is provided automatically when you create a Django project.
 
 That default model already includes:
-username, password,email, first_name, last_name, is_staff, is_superuser, date_joined, last_login, and more…
+username, password, email, first_name, last_name, is_staff, is_superuser, date_joined, last_login, and more…
 
 So basically, it’s a ready-to-use model for handling authentication, login, and user management.
 We can use Django’s built-in User model if we don’t need custom fields or roles & only need basic 
