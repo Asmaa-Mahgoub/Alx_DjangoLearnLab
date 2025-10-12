7908c37e2d1a2c6111bc7bb313d16a4aa847c82d
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-10-12 23:05:18 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
+from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticated
+from .models import User
 # Create your views here.
 
 class RegisterView(APIView):
@@ -27,3 +30,26 @@ class LoginView(APIView):
             token, _ = Token.objects.get_or_create(user=user)
             return Response({"token": token.key})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        target_user_id = kwargs.get('user_id')
+        try:
+            target_user = User.objects.get(id=target_user_id)
+            request.user.following.add(target_user)
+            return Response({'detail': f'You are now following {target_user.username}.'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        target_user_id = kwargs.get('user_id')
+        try:
+            target_user = User.objects.get(id=target_user_id)
+            request.user.following.remove(target_user)
+            return Response({'detail': f'You have unfollowed {target_user.username}.'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
