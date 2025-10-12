865fd03bf815545e0a14cd626fa133d8179c69d6
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-10-12 22:59:55 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, generics, permissions, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .models import Post, Comment
+from .models import Post, Comment, Like
+from notifications.models import Notification
 from .serializers import PostSerializer, CommentSerializer
 #from notifications.models import Notification
 
@@ -40,11 +41,11 @@ class FeedAPIView(generics.ListAPIView):
 
 # --- LIKE / UNLIKE VIEWS ---
 
-""" class LikePostView(generics.GenericAPIView):
+class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Posts, pk=pk)
+        post = get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post) 
 
         if created:
@@ -63,11 +64,11 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Posts, pk=pk)
+        post = get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post).first()
 
         if like:
             like.delete()
             return Response({'detail': 'Post unliked.'}, status=status.HTTP_200_OK)
 
-        return Response({'detail': 'You have not liked this post.'}, status=status.HTTP_400_BAD_REQUEST)"""
+        return Response({'detail': 'You have not liked this post.'}, status=status.HTTP_400_BAD_REQUEST)
