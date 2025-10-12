334fdbc2b125ef4a4bfd43bca622071df50bc7e6
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-10-12 23:19:01 +0300

    Amending in accounts.serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = get_user_model()
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'date_of_birth', 'location']
