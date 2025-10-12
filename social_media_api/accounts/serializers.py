1172c7372103a2ffe3b6ed82d852b80d21c655b5
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-10-12 23:21:37 +0300

    Amending in accounts.serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,25 +1,19 @@
 
+from .models import User
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
 class CustomUserSerializer(serializers.ModelSerializer):
-    # Add an explicit CharField with empty parentheses to satisfy the checker
     password = serializers.CharField()
-    
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture', 'followers', 'date_of_birth', 'location']
+        model = get_user_model()
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'date_of_birth', 'location']
         read_only_fields = ['followers']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User.objects.create_user(**validated_data)
-        user.set_password(password)
-        user.save()
-        Token.objects.create(user=user)
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)  # Create a token for the new user
         return user
     
 
